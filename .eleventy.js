f3203c0977b0963473bc8a3d4cd73b4e75529c3f
Rahul Setrakian <rahul.infosec@proton.me>
2026-07-01 16:42:38 +0530

    feat: complete Eleventy migration and premium redesign implementation

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+module.exports = function(eleventyConfig) {
+  // Pass through static assets
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/CNAME");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/sitemap.xml");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
+    },
+    templateFormats: ["njk", "html", "md"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
